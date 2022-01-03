c8511abe666b400ae3d483b61aaae2d7ae4e7f56
Brian Broll <brian.broll@gmail.com>
2022-01-03 16:21:19 -0600

    Fix configuration

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -6,17 +6,26 @@
 var config = require('webgme/config/config.default'),
     validateConfig = require('webgme/config/validator');
 
-
 // The paths can be loaded from the webgme-setup.json
-config.plugin.basePaths.push('src/plugins');
-config.visualization.decoratorPaths.push('src/decorators');
-config.seedProjects.basePaths.push('src/seeds/PetriNet');
+config.plugin.basePaths.push(__dirname + '/../src/plugins');
+config.visualization.decoratorPaths.push(__dirname + '/../src/decorators');
+config.seedProjects.basePaths.push(__dirname + '/../src/seeds/PetriNet');
+
+
+
+config.visualization.panelPaths.push(__dirname + '/../src/visualizers/panels');
+
+
 
 
 // Visualizer descriptors
-
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
-
+config.requirejsPaths = {
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets',
+  'petri-net': './src/common'
+};
 
 
 config.mongo.uri = 'mongodb://127.0.0.1:27017/petri_net';
